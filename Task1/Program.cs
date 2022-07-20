9fe0946b51cbe32f46e4df0d151672072196b853
lubov <loveavduhova@gmail.com>
2022-07-20 15:47:17 +0700

    homework done

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*  Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3 */
+
+
+int aboveZero = 0;
+int number = 0;
+Console.WriteLine("Если необходимо завершить программу, введите 1000");
+inputNumber();
+System.Console.WriteLine($"Вы ввели количество цифр больше нуля: {aboveZero}");
+
+
+void inputNumber ()
+{
+while (number != 1000)
+{
+    number = Prompt("Введите число: ");
+    if (number > 0 && number != 1000)
+        aboveZero++;
+}
+}
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 2.Напишите программу, которая найдёт точку пересечения двух прямых,
+ заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; -0,5) */
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+double resultEquationX(int b1, int b2, int k1, int k2)
+{
+    double x = (double)(b2 - b1) / (k1 - k2); // перевод в double, иначе выдает результат 0
+    return x;
+}
+
+double resultEquationY(int b1, int b2, int k1, int k2)
+{
+    double y = (double) k1 * resultEquationX (b1, b2, k1,k2)+ b1;
+    return y;
+}
+
+
+int b1 = Prompt("Введите b1: ");
+int k1 = Prompt("Введите k1: ");
+int b2 = Prompt("Введите b2: ");
+int k2 = Prompt("Введите k2: "); 
+
+System.Console.WriteLine($"X равно {resultEquationX(b1, b2, k1, k2)}, Y равно {resultEquationY(b1, b2, k1, k2)}");
